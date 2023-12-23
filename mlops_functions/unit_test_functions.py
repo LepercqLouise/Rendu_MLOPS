ac1f95f12b9f1bea85ac9b828a98dd80f700170c
cyrll28 <dfl.signature@gmail.com>
2023-12-23 13:43:26 +0100

    verification des test unitaires

--- mlops_functions/unit_test_functions.py
+++ mlops_functions/unit_test_functions.py
@@ -3,13 +3,13 @@ from sklearn.model_selection import train_test_split
 import unittest
 from functions import load_data, filter_summer_season, create_age_classes, create_height_classes, preprocess_data
 
-class TestYourFunctions(unittest.TestCase):
+class TestFunctions(unittest.TestCase):
     def setUp(self):
         # Mettez en place les données que vous utiliserez pour les tests
-        self.df = load_data('/Users/Cyrie/OneDrive/Bureau/TEST/athlete_events.csv')
+        self.df = load_data('/Users/Cyrie/OneDrive/Bureau/M2_DS/S1/MLOPS/Rendu_MLOPS/athlete_events.csv')
 
     def test_load_data(self):
-        loaded_data = load_data('/Users/Cyrie/OneDrive/Bureau/TEST/athlete_events.csv')
+        loaded_data = load_data('/Users/Cyrie/OneDrive/Bureau/M2_DS/S1/MLOPS/Rendu_MLOPS/athlete_events.csv')
         self.assertTrue(loaded_data.equals(self.df))
 
     def test_filter_summer_season(self):
@@ -18,12 +18,21 @@ class TestYourFunctions(unittest.TestCase):
 
     def test_create_age_classes(self):
         age_classes = create_age_classes(self.df)
-        self.assertEqual(age_classes['Classe_age'].nunique(), 4)
+        expected_classes = ['< 21 ans', '21 - 24 ans', '25-32 ans', '> 32 ans']
+        # Ignorez les valeurs 'nan' dans la comparaison
+        age_classes = set(age_classes['Classe_age'].dropna())
+        expected_classes = set(expected_classes)
+
+        self.assertEqual(age_classes, expected_classes, "Les classes d'âge ne correspondent pas.")
 
     def test_create_height_classes(self):
         height_classes = create_height_classes(self.df)
-        self.assertEqual(height_classes['Classe_height'].nunique(), 4)
+        expected_classes = ['< 165 cm', '165 - 172 cm', '173 - 185 cm', '> 185 cm']
+        # Ignorez les valeurs 'nan' dans la comparaison
+        height_classes = set(height_classes['Classe_height'].dropna())
+        expected_classes = set(expected_classes)
 
+        self.assertEqual(height_classes, expected_classes, "Les classes de hauteur ne correspondent pas.")
 
 if __name__ == '__main__':
     unittest.main()
